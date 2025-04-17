539342381bc1e14898586a232f57bd2b904b988b
kasilianaoliveira <kasilianaoliveira@gmail.com>
2025-04-17 15:14:42 -0300

    add database settings

---
+++ src/infra/core/connection.py
@@ -0,0 +1,26 @@
+import logging
+
+from dotenv import load_dotenv
+from sqlmodel import Session, SQLModel, create_engine
+
+from src.infra.core.settings import Settings
+
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
+
+load_dotenv()
+
+setting = Settings()
+engine = create_engine(
+    setting.DATABASE_URL, pool_pre_ping=True, echo=setting.TEST_MODE == "dev"
+)
+
+
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
--- src/models/enums.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class UserType(str, Enum):
-    ADMIN = "Admin"
-    RESPONSIBLE = "Responsible"
-    DEFAULT = "Default"
