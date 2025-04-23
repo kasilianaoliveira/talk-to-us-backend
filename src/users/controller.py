991900d214331fc56b550117c0aecf8bf21235d9
kasilianaoliveira <kasilianaoliveira@gmail.com>
2025-04-23 19:00:45 -0300

    fix: remove files

--- src/users/controller.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import Session
-
-from src.users.schema import UserSchema
-from src.users.service import create_user_service
-
-
-def create_user_controller(user_data: UserSchema, session: Session):
-    return create_user_service(user_data=user_data, session=session)
